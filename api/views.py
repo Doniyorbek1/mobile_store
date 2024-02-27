08630e45a13600d7539058295ff1d9acbac19a45
ZARIFMAC <uzbzarif@gmail.com>
2024-02-27 15:55:08 +0500

    CROS"

--- api/views.py
+++ api/views.py
@@ -6,6 +6,23 @@ from .models import Smartphones
 
 def add_product(reqeust: HttpRequest) -> JsonResponse:
     """add new product to database"""
-    print(reqeust.body)
+    # Check if request method is POST
+    if reqeust.method == 'POST':
+        # Get data from request
+        data = json.loads(reqeust.body)
+        print(data)
+        # Create new product
+        product = Smartphones(
+            price=data['price'],
+            img_url=data['img_url'],
+            color=data['color'],
+            ram=data['ram'],
+            memory=data['memory'],
+            name=data['name'],
+            model=data['model']
+        )
+        # Save product to database
+        product.save()
+        # Return response
     return JsonResponse({'status': 'ok'})
    
