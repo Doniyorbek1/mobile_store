93b3631c260c4622227b6e2b8e59a5ec9d3716b9
ZARIFMAC <uzbzarif@gmail.com>
2024-02-27 14:39:17 +0500

    add product route

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpRequest, JsonResponse
+from django.core.exceptions import ObjectDoesNotExist
+import json
+from .models import Smartphones
 
-# Create your views here.
+
+def add_product(reqeust: HttpRequest) -> JsonResponse:
+    """add new product to database"""
+    print(reqeust.body)
+    return JsonResponse({'status': 'ok'})
+   
