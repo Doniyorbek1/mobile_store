03d2437db07f742f53d166a8af5603ce6f86acb1
Doniyorbek <programmmerqety1@gmail.com>
2024-03-02 14:02:13 +0500

    urls add get product id

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from api.views import add_product, get_products
+from api.views import add_product, get_products, get_product_id
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/add', add_product),
     path('api/get', get_products),
+    path('api/get/<int:pk>', get_product_id),
 ]
