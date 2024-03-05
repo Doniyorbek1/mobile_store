a1d34fa0a2a4032345c8cb7ff684ec611964bef7
Doniyorbek <programmmerqety1@gmail.com>
2024-03-05 09:49:58 +0500

    model list

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from api.views import add_product, get_products, get_product_id,  get_name,  get_color
+from api.views import add_product, get_products, get_product_id,  get_name,  get_color,  lst_models
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/add', add_product),
     path('api/get', get_products),
     path('api/get/<int:pk>', get_product_id),
-    path('api/get/<str:name>', get_name),
+    path('api/get/name/<str:name>', get_name),
     path('api/get/color/<str:color>', get_color),
-    
-    
+    path('api/get/models_list', lst_models),   
 ]
